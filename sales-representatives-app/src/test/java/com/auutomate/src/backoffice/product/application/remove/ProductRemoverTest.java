c2add06a11cb3f83345d5af875f13291eff0c2e9
AnderGI <agibarguen@gmail.com>
2024-09-26 10:36:39 +0200

    Products backoffice delete added

--- sales-representatives-app/src/test/java/com/auutomate/src/backoffice/product/application/remove/ProductRemoverTest.java
+++ sales-representatives-app/src/test/java/com/auutomate/src/backoffice/product/application/remove/ProductRemoverTest.java
@@ -19,62 +19,63 @@ import com.auutomate.src.backoffice.product.domain.ProductReference;
 import com.auutomate.src.backoffice.product.domain.ProductReferenceMother;
 import com.auutomate.src.backoffice.product.domain.ProductRepository;
 import com.auutomate.src.backoffice.product.domain.remove.ProductRemovedDomainEvent;
-import com.auutomate.src.frontoffice.client.domain.Client;
 import com.auutomate.src.shared.aplication.find.ProductNotFoundException;
 import com.auutomate.src.shared.domain.EventBus;
 
 public class ProductRemoverTest {
-	private ProductRepository repo;
-	private EventBus bus;
-	private Product product;
-	private ProductRemovedDomainEvent event;
-	private ProductRemover remover;
-	private RemoveProductCommand command;
-	private RemoveProductCommandHandler handler;
-	
-	@BeforeEach
-	void setup() {
-		repo = Mockito.mock(ProductRepository.class);
-		bus = Mockito.mock(EventBus.class);
-		product = ProductMother.random();
-		event = new ProductRemovedDomainEvent(product.reference());
-		remover = new ProductRemover(bus, repo);
-		command = RemoveProductCommand.fromProduct(product);
-		handler = new RemoveProductCommandHandler(remover);
-	}
-	
-	@Test
-	void it_should_remove_a_product() throws ProductNotFoundException {
-		this.verifyRepoSearchReturn(ProductReferenceMother.create(product.reference()),Optional.ofNullable(product));
-		this.callUseCase();
-		this.verifyRepoRemoveCall();
-		this.verifyBusPublishCall();
-	}
+    private ProductRepository repo;
+    private EventBus bus;
+    private Product product;
+    private ProductRemover remover;
+    private RemoveProductCommand command;
+    private RemoveProductCommandHandler handler;
+    
+    @BeforeEach
+    void setup() {
+        repo = Mockito.mock(ProductRepository.class);
+        bus = Mockito.mock(EventBus.class);
+        product = ProductMother.random();
+        remover = new ProductRemover(bus, repo);
+        command = RemoveProductCommand.fromProduct(product);
+        handler = new RemoveProductCommandHandler(remover);
+    }
+    
+    @Test
+    void it_should_remove_a_product_and_publish_event() throws ProductNotFoundException {
+        this.mockRepoSearchReturn(ProductReferenceMother.create(product.reference()), Optional.of(product));
+        this.callUseCase();
+        this.verifyRepoRemoveCall();
+        this.verifyBusPublishCall();
+    }
 
-	@Test
-	void it_should_throw_an_exception_when_non_existing_product() {
-		this.verifyRepoSearchReturn(ProductReferenceMother.create(product.reference()),Optional.ofNullable(null));
-		assertThrows(ProductNotFoundException.class, () -> this.callUseCase());
-	}
-	
-	private void callUseCase() throws ProductNotFoundException {
-		handler.handle(command);
-	}
-	
-	private void verifyRepoSearchReturn(ProductReference reference, Optional<Product> product) {
-		when(repo.search(reference)).thenReturn(product);
-	}
-	
-	private void verifyRepoRemoveCall() {
-		ArgumentCaptor<ProductReference> referenceCaptor = ArgumentCaptor.forClass(ProductReference.class);
-		verify(repo, times(1)).remove(referenceCaptor.capture());
-		assertEquals(ProductReferenceMother.create(product.reference()), referenceCaptor.getValue());
-	}
+    @Test
+    void it_should_throw_an_exception_when_non_existing_product() {
+        this.mockRepoSearchReturn(ProductReferenceMother.create(product.reference()), Optional.empty());
+        assertThrows(ProductNotFoundException.class, () -> this.callUseCase());
+        verify(repo, times(0)).remove(Mockito.any());
+        verify(bus, times(0)).publish(Mockito.any());
+    }
+    
 
-	private void verifyBusPublishCall() {
-		ArgumentCaptor<ProductRemovedDomainEvent> eventCaptor = ArgumentCaptor.forClass(ProductRemovedDomainEvent.class);
-		verify(bus, times(1)).publish(eventCaptor.capture());
-		assertEquals(new ProductRemovedDomainEvent(product.reference()), eventCaptor.getValue());		
-	}
+    private void callUseCase() throws ProductNotFoundException {
+        handler.handle(command);
+    }
 
+    private void mockRepoSearchReturn(ProductReference reference, Optional<Product> product) {
+        when(repo.search(reference)).thenReturn(product);
+    }
+    
+    private void verifyRepoRemoveCall() {
+        ArgumentCaptor<ProductReference> referenceCaptor = ArgumentCaptor.forClass(ProductReference.class);
+        verify(repo, times(1)).remove(referenceCaptor.capture());
+        assertEquals(ProductReferenceMother.create(product.reference()), referenceCaptor.getValue());
+    }
+
+    private void verifyBusPublishCall() {
+        ArgumentCaptor<ProductRemovedDomainEvent> eventCaptor = ArgumentCaptor.forClass(ProductRemovedDomainEvent.class);
+        verify(bus, times(1)).publish(eventCaptor.capture());
+        
+        ProductRemovedDomainEvent capturedEvent = eventCaptor.getValue();
+        assertEquals(new ProductRemovedDomainEvent(product.reference()), capturedEvent);
+    }
 }
